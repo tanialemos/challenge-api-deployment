560e769dd46b7f2a934112f3ad625ccd311fa63e
tlemos <34248899+tanialemos@users.noreply.github.com>
2022-10-17 12:56:35 +0200

    check mandatory data

--- project/app.py
+++ project/app.py
@@ -1,1 +1,39 @@
-print("gello world")
+import json
+
+"""
+# deserialising json
+    property = json.loads(api_input)
+"""
+
+'''
+INPUT JSON
+{
+  "data": {
+    "area": int,
+    "property-type": "APARTMENT" | "HOUSE" | "OTHERS",
+    "rooms-number": int,
+    "zip-code": int,
+    "land-area": Optional[int],
+    "garden": Optional[bool],
+    "garden-area": Optional[int],
+    "equipped-kitchen": Optional[bool],
+    "full-address": Optional[str],
+    "swimming-pool": Optional[bool],
+    "furnished": Optional[bool],
+    "open-fire": Optional[bool],
+    "terrace": Optional[bool],
+    "terrace-area": Optional[int],
+    "facades-number": Optional[int],
+    "building-state": Optional[
+      "NEW" | "GOOD" | "TO RENOVATE" | "JUST RENOVATED" | "TO REBUILD"
+    ]
+  }
+}
+
+OUTPUT JSON
+{
+  "prediction": Optional[float],
+  "status_code": Optional[int]
+}
+
+'''
---
+++ project/predict/prediction.py
@@ -0,0 +1,9 @@
+
+
+def predict():
+    # takes property object as arg?
+    # runs it through the model ?
+    # receives price object from model ?
+    # returns price json ??
+
+    pass
